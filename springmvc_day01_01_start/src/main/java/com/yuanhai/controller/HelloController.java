6396a10effd23c2cadc01bfcb8b7bbe29af5a813
yuanhai <504891464@qq.com>
2020-11-30 23:10:26 +0800

    springmvc入门案例的代码编写~

---
+++ springmvc_day01_01_start/src/main/java/com/yuanhai/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.yuanhai.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 控制器类
+ * @author yuanhai
+ * @date 2020/11/30
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping(path = "/hello")
+    public String sayHello(){
+        System.out.println("Hello,SpringMVC!  ！~！");
+        return "success";
+    }
+}
--- springmvc_day01_01_start/src/main/webapp/WEB-INF/pages/success.jsp
+++ springmvc_day01_01_start/src/main/webapp/WEB-INF/pages/success.jsp
@@ -2,19 +2,15 @@
   Created by IntelliJ IDEA.
   User: yuanhai
   Date: 2020/11/30
-  Time: 22:09
+  Time: 22:49
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>跳转成功~！</title>
 </head>
 <body>
-
-    <h3>入门程序</h3>
-
-    <a href="hello">入门程序</a>
 
 </body>
 </html>
