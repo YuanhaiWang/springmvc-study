cbb03c4df4a8f9e92ae22cb18db2adcea8aab56f
yuanhai <504891464@qq.com>
2020-12-23 22:28:42 +0800

    springmvc:springmvc方式的文件上传

--- springmvc_day02_02_fileupload/src/main/java/com/yuanhai/controller/FileUploadController.java
+++ springmvc_day02_02_fileupload/src/main/java/com/yuanhai/controller/FileUploadController.java
@@ -6,9 +6,11 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,6 +22,12 @@ import java.util.UUID;
 @RequestMapping("/fileUpload")
 public class FileUploadController {
 
+    /**
+     * 传统方式文件上传
+     * @param request
+     * @return
+     * @throws Exception
+     */
     @RequestMapping("/fileUpload1")
     public String fileUpload1(HttpServletRequest request) throws Exception {
         System.out.println("fileUpload1 run...");
@@ -58,6 +66,39 @@ public class FileUploadController {
                 item.delete();
             }
         }
+
+        return "success";
+    }
+
+    /**
+     * springmvc方式文件上传
+     * 方法接收MultipartFile对象的名字，必须和前端表单的名字一样，所以这里对象名字是upload
+     * @param upload
+     * @param request
+     * @return
+     */
+    @RequestMapping("/fileUpload2")
+    public String fileUpload2(HttpServletRequest request,MultipartFile upload) throws IOException {
+        System.out.println("springmvc方式的文件上传...");
+
+        // 使用fileupload组件完成文件上传
+        // 文件上传的位置
+        System.out.println("getContextPath : "+request.getSession().getServletContext().getContextPath());
+        String path = request.getSession().getServletContext().getRealPath("/uploads/");
+        // 判断该路径是否存在
+        File file =new File(path);
+        if(!file.exists()){
+            // 创建该文件夹
+            file.mkdirs();
+        }
+
+        // 获取上传文件的名称
+        String fileName = upload.getOriginalFilename();
+        // 把文件名称设置成唯一值,uuid
+        String uuid = UUID.randomUUID().toString().replace("-","");
+        fileName = uuid+"_"+fileName;
+        // 完成文件上传
+        upload.transferTo(new File(path,fileName));
 
         return "success";
     }
--- springmvc_day02_02_fileupload/src/main/webapp/index.jsp
+++ springmvc_day02_02_fileupload/src/main/webapp/index.jsp
@@ -14,8 +14,17 @@
 
 
     <h3>文件上传</h3>
-
+    -------------------------------------------------<br>
+    <h3>传统方式文件上传</h3>
     <form action="fileUpload/fileUpload1" method="post" enctype="multipart/form-data">
+        选择文件：<input type="file" name="upload" /> <br>
+        <input type="submit" value="上传">
+    </form>
+
+    -------------------------------------------------<br>
+
+    <h3>springmvc方式文件上传</h3>
+    <form action="fileUpload/fileUpload2" method="post" enctype="multipart/form-data">
         选择文件：<input type="file" name="upload" /> <br>
         <input type="submit" value="上传">
     </form>
